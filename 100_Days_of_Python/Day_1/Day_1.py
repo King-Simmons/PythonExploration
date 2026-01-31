6db87c2b9abbda4727c8043d93ecd42fad412716
kingsimmons <kd3business@gmail.com>
2026-01-30 18:39:34 -0600

    Day 1/100 - Done

---
+++ 100_Days_of_Python/Day_1/Day_1.py
@@ -0,0 +1,8 @@
+greeting = "Welcome to the DAY 1: Band Name Generator Project."
+question1 = "What city did you grow up in?\n"
+question2 = "What is the name of a pet or a store?\n"
+
+print(greeting)
+city = input(question1)
+pet_or_store = input(question2)
+print(f"Your Band Name is {city} {pet_or_store}!!")
